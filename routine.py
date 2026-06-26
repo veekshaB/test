f42a45c189c5905fcddaccc2966c1a22753ef5f7
veekshaB <pengunti@gmail.com>
2026-06-25 23:35:55 -0400

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,18 @@
+print("my daily routine")
+# step one:waking up
+print("I wake up at 7:30")
+
+#step two: go to school
+print("I go to school at 9:00")
+
+#step three: come home
+print("I come home at 3:45")
+
+#step four: study
+print("I study for 30 minutes")
+
+#step five: go to park
+print("I go to the park for 2 hours")
+
+# step 6: calculating total study time in a week
+print("total study minutes a week:", 7*30)
